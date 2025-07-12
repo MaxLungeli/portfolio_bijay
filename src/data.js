9beb26a5c6baac23b8e5693cfa0ca46731dc1c8d
Rohit <rohitlungeli30@gmail.com>
2025-07-12 13:44:03 +0545

    adding data and edit folders

---
+++ src/data.js
@@ -0,0 +1,7 @@
+export const NavData = [
+  { id: 1, body: "home" },
+  { id: 2, body: "about" },
+  { id: 1, body: "portfolilo" },
+  { id: 1, body: "contact me" },
+  { id: 1, body: "book online" },
+];
