d23775040e2f878be0cc2ef35a6d25f2791ff0bf
Rohit <rohitlungeli30@gmail.com>
2025-10-13 08:39:41 +0545

    remove: delete unused video and image assets

--- src/assets/videos/index.js
+++ src/assets/videos/index.js
@@ -1,36 +1,13 @@
 import mr_beast from "./mr_beast.mp4";
-import ai from "./ai.mp4";
-import clz from "./clz.mp4";
-import book from "./book.mp4";
-import dashain from "./dashain.mp4";
-import dtour2 from "./dtour2.mp4";
-import dtour3 from "./dtour3.mp4";
-import dtours from "./dtours.mp4";
-import nepal_tour from "./nepal_tour.mp4";
-import travels from "./travels.mp4";
 
-import aipc from "./pictures/aipc.jpeg";
-import bookpc from "./pictures/bookpc.jpeg";
-import clzpc from "./pictures/clzpc.jpeg";
-import dashainpc from "./pictures/dashainpc.jpeg";
-import dtour2pc from "./pictures/dtour2pc.jpeg";
+import dtour3 from "./dtour3.mp4";
+
 import dtour3pc from "./pictures/dtour3pc.jpeg";
-import dtourspc from "./pictures/dtourspc.jpeg";
 import mr_bistpc from "./pictures/mr_bistpc.jpeg";
-import nepal_tourpc from "./pictures/nepal_tourpc.jpeg";
-import travelspc from "./pictures/travelspc.jpeg";
 
 const videosData = [
-  { id: 4, src: book, poster: bookpc },
-  { id: 5, src: dashain, poster: dashainpc },
   { id: 1, src: mr_beast, poster: mr_bistpc },
-  { id: 2, src: ai, poster: aipc },
   { id: 7, src: dtour3, poster: dtour3pc },
-  { id: 8, src: dtours, poster: dtourspc },
-  { id: 6, src: dtour2, poster: dtour2pc },
-  { id: 9, src: nepal_tour, poster: nepal_tourpc },
-  { id: 10, src: travels, poster: travelspc },
-  { id: 3, src: clz, poster: clzpc },
 ];
 
 export default videosData;
