48c00e250b7473ab20aa4fedd5f910fb3e5a5300
Rohit <rohitlungeli30@gmail.com>
2025-10-13 08:45:28 +0545

    remove: delete unused video imports and commented-out YouTube iframe

--- src/assets/other/data.js
+++ src/assets/other/data.js
@@ -1,7 +1,6 @@
 import ai from "../videos/ai.mp4";
-import mr_beast from "../videos/mr_beast.mp4";
+
 import ai_pc from "../videos/pictures/aipc.jpeg";
-import mr_beast_pc from "../videos/pictures/mr_bistpc.jpeg";
 
 import boudha from "../videos/boudha.mp4";
 import boudhapc from "../videos/pictures/boudhapc.jpeg";
@@ -12,7 +11,6 @@ export const HomeDataVid = [
   { id: 3, src: amazon_vid, poster: amazon_poster },
   { id: 1, src: boudha, poster: boudhapc },
   { id: 2, src: ai, poster: ai_pc },
-  // { id: 3, src: mr_beast, poster: mr_beast_pc },
 ];
 
 export const youTubeData = [
@@ -22,10 +20,4 @@ export const youTubeData = [
     title:
       "⁠Luv Kush Ki Janmabhoomi: Ramayan Ka Jeevit Itihaas | By Akshat Gupta | हिंदी में",
   },
-  // {
-  //   id: 2,
-  //   src: "https://www.youtube.com/embed/zsb5dl1isgI",
-  //   title:
-  //     "Janiye kya hota hai &#39;Panchakshari Mantra&#39; #akshatguptapodcast #akshatgupta #lordshiva #shravanmaas",
-  // },
 ];
--- src/components/utility/YouTube.jsx
+++ src/components/utility/YouTube.jsx
@@ -13,18 +13,6 @@ const YouTube = ({ src, title }) => {
           style={{ width: "100%", height: "100%" }}
         ></iframe>
       </div>
-      {/* <div className="video_container">
-        <iframe
-          style={{ width: "100%" }}
-          src="https://www.youtube.com/embed/zsb5dl1isgI"
-          title="Janiye kya hota hai &#39;Panchakshari Mantra&#39; #akshatguptapodcast #akshatgupta #lordshiva #shravanmaas"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-          className="video_elem"
-        ></iframe>
-      </div> */}
     </div>
   );
 };
